2f950132faffb11233ecf80023042d4b378c4a4a
Mohamed Belkamel <belkamelmohamed@gmail.com>
2020-11-29 18:19:32 +0100

    exerice2 tp 2

---
+++ exercice2_tp.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class Point
+{
+private:
+    /* data */
+public:
+    Point(/* args */);
+    ~Point();
+};
+
+Point::Point(/* args */)
+{
+}
+
+Point::~Point()
+{
+}
+
+int main()
+{
+    Point personne;
+
+    return 0;
+}
